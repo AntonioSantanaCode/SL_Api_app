5f686b5f2573251a5b69c1f6b3a21cb130945f06
AntonioSantanaCode <71064079+AntonioSantanaCode@users.noreply.github.com>
2021-03-16 14:49:14 +0100

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SL2.Models;
+using SL2.Models.Departure;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text;
+using Microsoft.AspNetCore.Http;
+
+namespace SL2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public HttpClient client = new HttpClient();
+        public Stops hpl { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string siteId { get; set; }
+
+        public Departures departure { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public string location { get; set; }
+
+        [BindProperty(SupportsGet = true)]
+        public int minToStations { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        } 
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            if(location != null)
+            {
+                HttpContext.Session.SetString("location", location);
+            }
+            else
+            {
+                if(HttpContext.Session.GetString("location") !=null)
+                {
+                    location = HttpContext.Session.GetString("location");
+                }
+                else
+                {
+                    location = "";
+                }
+            }
+
+            if (minToStations != 0)
+            {
+                HttpContext.Session.SetInt32("minToStations", minToStations);
+            }
+            else
+            {
+                if (HttpContext.Session.GetString("minToStations") != null)
+                {
+                    minToStations = (int)HttpContext.Session.GetInt32("minToStations");
+                }
+                else
+                {
+                    minToStations = 0;
+                }
+            }
+
+            hpl = await GetStopsAsync();
+            departure = await GetDeparturesAsync();
+            return Page();
+        }
+
+        public async Task<Stops> GetStopsAsync()
+        {
+            string link = $"https://api.sl.se/api2/typeahead.json?key=1c61434d648d4d039178bcb7ad7181a4&searchstring={location}&maxresults=10";
+            Task<string> getBusstopStringTask = client.GetStringAsync(link);
+            string busStopString = await getBusstopStringTask;
+            hpl = JsonSerializer.Deserialize<Stops>(busStopString);
+            return hpl;
+        }
+
+        public async Task<Departures> GetDeparturesAsync()
+        {
+            string link = $"https://api.sl.se/api2/realtimedeparturesV4.json?key=acf6c15d6216484c8c4895597d6b9797&siteid={siteId}&timewindow=60";
+            Task<string> getBusDepartureStringTask = client.GetStringAsync(link);
+            string busDepartureString = await getBusDepartureStringTask;
+            departure = JsonSerializer.Deserialize<Departures>(busDepartureString);
+            return departure;
+        }
+
+
+    }
+}
